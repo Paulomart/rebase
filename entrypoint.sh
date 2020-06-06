1df0838c929cd73bb87fd2eec169fc1943412e0d
Paul Heidenreich <paul@paul-h.de>
2020-06-06 22:24:57 +0200

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 set -o xtrace
 
-PR_NUMBER=$(jq -r ".issue.number" "$GITHUB_EVENT_PATH")
+PR_NUMBER=$(jq -r ".number" "$GITHUB_EVENT_PATH")
 
 echo "Collecting information about PR #$PR_NUMBER of $GITHUB_REPOSITORY..."
 
